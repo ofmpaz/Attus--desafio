0b9cc57d6adadf9abe3accea3274245888e8137d
Maria Andrade <deise_andrade_paz@hotmail.com>
2024-05-03 17:56:28 -0300

    [feat]: Primeiro commit

---
+++ src/main/java/com/desafioattus/DesafioAttusApplication.java
@@ -0,0 +1,13 @@
+package com.desafioattus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAttusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAttusApplication.class, args);
+	}
+
+}
