3344f01d512c711b017138779788a073907d9705
Maria Andrade <deise_andrade_paz@hotmail.com>
2024-05-03 23:33:00 -0300

    [feat] Interface - BuscaPessoaNome

--- src/main/java/com/desafioattus/repository/PessoaRepository.java
+++ src/main/java/com/desafioattus/repository/PessoaRepository.java
@@ -1,10 +1,17 @@
 package com.desafioattus.repository;
 
 import com.desafioattus.model.Pessoa;
+import com.desafioattus.model.dto.PessoaDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, UUID> {
+
+    @Query("SELECT new com.desafioattus.model.dto.PessoaDTO(p.id, p.nome, p.dataNascimento) FROM Pessoa p WHERE p.nome LIKE %:nome%")
+    List<PessoaDTO> findDTOByNome(@Param("nome") String nome);
 
 }
