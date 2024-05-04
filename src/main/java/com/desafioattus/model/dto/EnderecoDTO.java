dcfa0ba81865d777f4da3d10fff381b47b2719ea
Maria Andrade <deise_andrade_paz@hotmail.com>
2024-05-04 00:46:53 -0300

    [feat] EnderecoDTO

---
+++ src/main/java/com/desafioattus/model/dto/EnderecoDTO.java
@@ -0,0 +1,34 @@
+package com.desafioattus.model.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EnderecoDTO {
+
+    private UUID id;
+
+    @NotBlank(message = "Logradouro é obrigatório")
+    private String logradouro;
+
+    @NotBlank(message = "Cep é obrigatório")
+    private String cep;
+    
+    private int numero;
+
+    @NotBlank(message = "Cidade é obrigatório")
+    private String cidade;
+
+    @NotBlank(message = "Estado é obrigatório")
+    private String estado;
+}
