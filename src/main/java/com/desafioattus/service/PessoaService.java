502c1de79d3e02144a579746c340110d90cf6a25
Maria Andrade <deise_andrade_paz@hotmail.com>
2024-05-03 23:03:06 -0300

    [feat]: PessoaService

--- src/main/java/com/desafioattus/service/PessoaService.java
+++ src/main/java/com/desafioattus/service/PessoaService.java
@@ -1,4 +1,30 @@
 package com.desafioattus.service;
 
+import com.desafioattus.mapper.PessoaMapper;
+import com.desafioattus.model.Pessoa;
+import com.desafioattus.model.dto.PessoaDTO;
+import com.desafioattus.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PessoaService {
+
+    @Autowired
+    PessoaRepository pessoaRepository;
+
+    @Autowired
+    PessoaMapper pessoaMapper;
+
+    public PessoaDTO adicionarPessoa(PessoaDTO pessoaDTO) {
+
+        Pessoa pessoa = pessoaMapper.paraPessoa(pessoaDTO);
+
+        Pessoa registraPessoa = pessoaRepository.save(pessoa);
+
+        PessoaDTO registrarPessoaDTO = pessoaMapper.paraPessoaDTO(registraPessoa);
+
+        return registrarPessoaDTO;
+    }
+
 }
