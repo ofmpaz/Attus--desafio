2b374b3d33ae195e39bc76a8f32fad430d66127d
Maria Andrade <deise_andrade_paz@hotmail.com>
2024-05-04 00:46:41 -0300

    [feat] Endereco entidade

---
+++ src/main/java/com/desafioattus/model/Endereco.java
@@ -0,0 +1,30 @@
+package com.desafioattus.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.UUID;
+
+@Entity
+@Table(schema = "SYS", name = "TB_ENDERECO")
+@Data
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "Logradouro", nullable = false)
+    private String logradouro;
+
+    @Column(name = "Cep", nullable = false)
+    private String cep;
+
+    @Column(name = "numero")
+    private int numero;
+
+    @Column(name = "Cidade", nullable = false)
+    private String cidade;
+
+    @Column(name = "Estado", nullable = false)
+    private String estado;
+}
